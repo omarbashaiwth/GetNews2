e1299dfb2ea361e2b85dfaceaca7b642d6e303f9
om2013ab <60521259+om2013ab@users.noreply.github.com>
2021-05-01 14:35:10 +0800

    Delete Glide import

--- app/src/main/java/com/omarahmed/getnews2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/omarahmed/getnews2/ui/home/HomeFragment.kt
@@ -7,20 +7,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.CompositePageTransformer
-import androidx.viewpager2.widget.MarginPageTransformer
 import com.omarahmed.getnews2.R
 import com.omarahmed.getnews2.databinding.FragmentHomeBinding
-import com.omarahmed.getnews2.util.Resource
-import com.omarahmed.getnews2.util.showNews
-import com.omarahmed.getnews2.util.showShareBottomSheet
-import com.omarahmed.getnews2.util.showSnackBar
+import com.omarahmed.getnews2.util.*
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import java.util.*
-import kotlin.math.abs
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
@@ -50,25 +43,12 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 layoutManager = GridLayoutManager(requireContext(), 2)
             }
             vpForYouNews.apply {
-                val compositePaTransformer = CompositePageTransformer()
-                compositePaTransformer.apply {
-                    addTransformer(MarginPageTransformer(30))
-                    addTransformer { page, position ->
-                        val r = 1 - abs(position)
-                        page.scaleY = 0.85f + r * 0.14f
-                    }
-                }
                 adapter = viewPagerAdapter
-                offscreenPageLimit = 3
-                getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
-                setPageTransformer(compositePaTransformer)
+                setPageTransformer(3,0)
             }
-            viewLifecycleOwner.lifecycleScope.launchWhenCreated {
-                homeViewModel.getLatestNews.collect {
-                    val result = it ?:return@collect
-                    swipeRefreshLayout.isRefreshing = result is Resource.Loading
-                    progressBar.isVisible = result is Resource.Loading && result.data.isNullOrEmpty()
-                    tvLatestNewsTitle.isVisible = result is Resource.Loading || !result.data.isNullOrEmpty()
+            swipeRefreshLayout.setOnRefreshListener {
+                homeViewModel.onManualRefresh()
+            }
 
                     latestNewsAdapter.submitList(result.data){
                         if (homeViewModel.pendingScrollToTopAfterRefresh){
@@ -88,8 +68,9 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 tvForYouTitle.isVisible = result is Resource.Loading || !result.data.isNullOrEmpty()
 
             }
+
         }
-        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             homeViewModel.newsEvents.collect { event ->
                 when (event) {
                     is HomeViewModel.NewsEvents.ShowBookmarkedMessage -> {
